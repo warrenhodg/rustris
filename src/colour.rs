20035fe579ec54842f264692d61c657898c9a72d
Warren Hodgkinson <warren.hodgkinson@gmail.com>
2020-04-30 07:10:07 +0100

    Added Wall Colour

--- src/colour.rs
+++ src/colour.rs
@@ -3,6 +3,7 @@ use std::fmt;
 #[derive(Copy, Clone, PartialEq)]
 pub enum Colour {
     Empty,
+    Wall,
     Value(usize),
 }
 
@@ -10,6 +11,7 @@ impl fmt::Debug for Colour {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Colour::Empty => write!(f, "empty"),
+            Colour::Wall => write!(f, "wall"),
             Colour::Value(v) => write!(f, "#{0}", v),
         }
     }
@@ -19,6 +21,7 @@ impl fmt::Display for Colour {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Colour::Empty => write!(f, "empty"),
+            Colour::Wall => write!(f, "wall"),
             Colour::Value(v) => write!(f, "#{0}", v),
         }
     }
